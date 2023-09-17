6f5986c2a18c9c0fe9b41ef259cb752bf3c2bd50
p.babaali <pouriya.babaali1998@gmail.com>
2023-09-17 17:48:42 +0330

    remove comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,6 @@ import { TelegramModule } from './providers/bot/node-telegram-bot-api/telegram/t
 @Module({
   imports: [
     ConfigModule.forRoot({
-      // envFilePath: `${process.cwd()}/config/env/${process.env.NODE_ENV}.env`,
       load: [configuration],
       isGlobal: true,
     }),
