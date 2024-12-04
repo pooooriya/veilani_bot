06d1385242bdb726eabc4a9a4ba4295341d8e7fa
pouriya <pouriya.babaali1998@gmail.com>
2024-12-04 23:15:40 +0330

    fix

--- src/providers/bot/node-telegram-bot-api/telegram/telegram.service.ts
+++ src/providers/bot/node-telegram-bot-api/telegram/telegram.service.ts
@@ -20,11 +20,12 @@ export class TelegramService implements ITelegramService {
 
   private handleServerCommand = async (message: TelegramBot.Message) => {
     try {
-      const serverDetails = '```connect 5.57.32.32:28441;password veilani```';
+      const serverDetails = 'connect 5.57.32.32:28441;password veilani';
 
       this.bot.sendMessage(
         message.chat.id,
-        serverDetails,
+        `\`\`\`${serverDetails}\`\`\``,
+        { parse_mode: 'MarkdownV2' }
       );
     } catch (error) {
       this.logger.error('Server command failed ====>', error);
