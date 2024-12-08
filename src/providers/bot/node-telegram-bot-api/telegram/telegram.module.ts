24c6e9f5a805f94aa1859c34ed078c4a242d3fac
pouriya <pouriya.babaali1998@gmail.com>
2024-12-08 21:39:50 +0330

    add

--- src/providers/bot/node-telegram-bot-api/telegram/telegram.module.ts
+++ src/providers/bot/node-telegram-bot-api/telegram/telegram.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+  ],
   providers: [TelegramService],
   exports: [TelegramService],
 })
